e21290ea2d76ad70a79d77139fe28555047d48c9
balachandar <balachandark.bsf.io>
2020-01-04 21:57:58 +0530

    Updates

---
+++ precache-manifest.42220f4701a654c831e0149cfcc93a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767ff3ce8b736706a347d3a1a078fc0b",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "ee1cb72bbdde8c4bbff0",
+    "url": "/react-calculator/static/css/main.e29939bc.chunk.css"
+  },
+  {
+    "revision": "a665f402b13a02dcad11",
+    "url": "/react-calculator/static/js/2.fd32c8ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-calculator/static/js/2.fd32c8ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee1cb72bbdde8c4bbff0",
+    "url": "/react-calculator/static/js/main.abfe95bd.chunk.js"
+  },
+  {
+    "revision": "600698fe122fa8276e83",
+    "url": "/react-calculator/static/js/runtime-main.e60f86e9.js"
+  }
+]);
